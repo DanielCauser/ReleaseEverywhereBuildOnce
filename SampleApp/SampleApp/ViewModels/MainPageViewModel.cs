79be1b141dbc3d733c08da55835f2055c11b61ba
Daniel Causer <danielcauser@gmail.com>
2019-08-17 18:54:45 -0400

    Basic app and environment endpoints

--- SampleApp/SampleApp/ViewModels/MainPageViewModel.cs
+++ SampleApp/SampleApp/ViewModels/MainPageViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SampleApp.ViewModels
 {
@@ -14,6 +15,23 @@ namespace SampleApp.ViewModels
             : base(navigationService)
         {
             Title = "Main Page";
+            EnvironmentConfig = "Production";
+            GetEnvironmentInfoCommand = new DelegateCommand(async () => await GetInfo());
+        }
+
+        private Task GetInfo()
+        {
+            EnvironmentConfig = "Prod";
+            return Task.CompletedTask;
+        }
+
+        public DelegateCommand GetEnvironmentInfoCommand { get; private set; }
+
+        private string _environmentConfig;
+        public string EnvironmentConfig
+        {
+            get { return _environmentConfig; }
+            set { SetProperty(ref _environmentConfig, value); }
         }
     }
 }
