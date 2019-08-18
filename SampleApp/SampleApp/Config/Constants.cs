1ed7c127d823dd1c487160de7295ad8b5a6a813b
Daniel Causer <danielcauser@gmail.com>
2019-08-17 20:30:37 -0400

    app settings setup

---
+++ SampleApp/SampleApp/Config/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SampleApp.Config
+{
+    public static class Constants
+    {
+        public static string EndPoint =>
+#if DEBUG
+        AppSettingsManager.Settings["appsettings.dev.json"];
+#else
+        AppSettingsManager.Settings["appsettings.json"];
+#endif
+
+    }
+}
