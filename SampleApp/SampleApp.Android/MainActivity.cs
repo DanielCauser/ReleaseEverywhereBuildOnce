8a93a8067118c88158344532b2c897984f1bf1e6
Daniel Causer <danielcauser@gmail.com>
2019-10-27 13:23:23 -0400

    Android Icons

--- SampleApp/SampleApp.Android/MainActivity.cs
+++ SampleApp/SampleApp.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Prism.Ioc;
 
 namespace SampleApp.Droid
 {
-    [Activity(Label = "SampleApp", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CauserException", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
