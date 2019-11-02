ae8566b120882fe7b9244144fa210a55ddf9bb48
Daniel Causer <danielcauser@gmail.com>
2019-11-02 19:51:07 -0400

    adding icons and change to about

--- CauserException/CauserException.Android/MainActivity.cs
+++ CauserException/CauserException.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CauserException.Droid
 {
-    [Activity(Label = "CauserException", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CauserException", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
